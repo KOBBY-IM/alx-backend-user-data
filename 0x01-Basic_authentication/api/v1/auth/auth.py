66b43f097c9a80f79748eac4abcc5da2837301ee
KOBBY_IM <nyarkotheo2@gmail.com>
2024-04-16 23:58:54 +0000

    imporved def require

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,21 +8,18 @@ from typing import List, TypeVar
 class Auth:
     """ auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ require auth
-        """
-        if excluded_paths is None or excluded_paths == []:
+        """Require authentication for a given path, except for excluded paths."""
+        if not excluded_paths:
             return True
+        
         for excluded_path in excluded_paths:
-            if excluded_path[-1] != '/':
-                excluded_path += '/'
-        if path is None:
-            return True
-        if path[-1] != '/':
-            path += '/'
-        if path not in excluded_paths:
-            return True
-        return False
-
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path:
+                return False
+        
+        return True
     def authorization_header(self, request=None) -> str:
         """ authorization header"""
         if request is None:
